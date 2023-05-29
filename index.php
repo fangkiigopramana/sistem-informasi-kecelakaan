df69fe1483e8a7ec1b9e80723f3cc953dd612a3f
Fangki Igo Pramana <fangkiigo26@gmail.com>
2023-05-29 20:47:18 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,326 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>InfoKecelakaan</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Bootstrap icons-->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" /> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/sb-admin-2.css" rel="stylesheet" />
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body class="d-flex flex-column h-100">
+    <main class="flex-shrink-0">
+
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
+            <div class="container px-5">
+                <a class="navbar-brand fw-bold" href="index.html">InfoKecelakaan</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation"><span
+                        class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0 fw-bold" style="margin-right: 30px;">
+                        <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="pricing.html">Pemicu</a></li>
+                        <li class="nav-item"><a class="nav-link" href="about.html">Statistik</a></li>
+                        <li class="nav-item"><a class="nav-link" href="contact.html">Penanganan</a></li>
+                    </ul>
+                    <a href="admin/login.php">
+                        <button class="btn btn-outline-light me-2" type="button">Login Admin</button>
+                    </a>
+                </div>
+            </div>
+        </nav>
+
+        <!-- Header-->
+        <header style="background-image: url('assets/bg-lalu-lintas.jpg')">
+            <div class="bg-dark  bg-opacity-75 py-5">
+                <div class="container px-5 opacity-100">
+                    <div class="row gx-5 align-items-center justify-content-center">
+                        <div class="col-lg-8 col-xl-7 col-xxl-6">
+                            <div class="my-5 text-center text-xl-start">
+                                <h1 class="display-5 fw-bolder text-white mb-2">
+                                    Perbanyak Pengetahuan, Kurangi Kecelakaan
+                                </h1>
+                                <p class="lead fw-normal text-white-50 mb-4">Menyediakan berbagai informasi tentang
+                                    kecelakaan dan cara mencegahnya, sehinggak kamu dapat meningkatkan pengetahuan dan
+                                    mengurangi risiko terjadinya kecelakaan.</p>
+                                <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
+                                    <a class="btn btn-warning btn-lg px-4 me-sm-3" href="#features">Lihat Statistik</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center">
+                            <img class="img-fluid rounded-3 my-5" src="assets/icon-kendaraan.png"
+                                alt="..." />
+                        </div>
+                    </div>
+                </div>
+        </header>
+
+        <!-- Pemicu section-->
+        <section class="py-5" id="pemicu">
+            <div class="container px-5 my-5">
+                <div class="row gx-5">
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h2 class="fw-bolder mb-4 justify-content-between">Apa saja penyebab terjadinya sebuah
+                            kecelakaan?</h2>
+                        <img src="https://img.beritasatu.com/cache/beritasatu/910x580-2/1569903660.jpg"
+                            class="rounded float-start" alt="" width="100%">
+                    </div>
+                    <div class="col-lg-8">
+                        <div class="row gx-5 row-cols-1 row-cols-md-2">
+                            <div class="col mb-5 h-100">
+                                <div class="feature bg-warning bg-gradient text-black fw-bold rounded-3 mb-3">
+                                    1
+                                </div>
+                                <h2 class="h5">Featured title</h2>
+                                <p class="mb-0">Paragraph of text beneath the heading to explain the heading. Here is
+                                    just a bit more text.</p>
+                            </div>
+                            <div class="col mb-5 h-100">
+                                <div class="feature bg-warning bg-gradient text-black fw-bold rounded-3 mb-3">
+                                    2
+                                </div>
+                                <h2 class="h5">Featured title</h2>
+                                <p class="mb-0">Paragraph of text beneath the heading to explain the heading. Here is
+                                    just a bit more text.</p>
+                            </div>
+                            <div class="col mb-5 mb-md-0 h-100">
+                                <div class="feature bg-warning bg-gradient text-black fw-bold rounded-3 mb-3">
+                                    3
+                                </div>
+                                <h2 class="h5">Featured title</h2>
+                                <p class="mb-0">Paragraph of text beneath the heading to explain the heading. Here is
+                                    just a bit more text.</p>
+                            </div>
+                            <div class="col h-100">
+                                <div class="feature bg-warning bg-gradient text-black fw-bold rounded-3 mb-3">
+                                    4
+                                </div>
+                                <h2 class="h5">Featured title</h2>
+                                <p class="mb-0">Paragraph of text beneath the heading to explain the heading. Here is
+                                    just a bit more text.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Quotes section-->
+        <div class="py-5 bg-warning bg-opacity-25">
+            <div class="container px-5 my-5">
+                <div class="row gx-5 justify-content-center">
+                    <div class="col-lg-10 col-xl-7">
+                        <div class="text-center">
+                            <div class="fs-4 mb-4 fst-italic">"Kecelakaan bukanlah suatu kejadian yang terjadi pada
+                                orang lain, tapi bisa terjadi pada siapa saja. Oleh karena itu, waspada dan hati-hati
+                                selalu saat berkendara."</div>
+                            <div class="d-flex align-items-center justify-content-center">
+                                <div class="fw-bold fst-italic">
+                                    Open AI
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Statistik section -->
+        <div class="py-5 container">
+            <h2 class="fw-bolder mb-0 text-center">Kita lihat data kecelakaan di Indonesia</h2>
+            <div class="row">
+                <div class="my-auto">
+                    <p class="text-right text-lg-center fst-italic p-lg-5">Kami telah melakukan analisis terhadap data
+                        kecelakaan yang terjadi di beberapa daerah Indonesia. <br>Melalui data grafik ini, diharapkan
+                        mendapat pemahaman tentang pentingnya menjaga keselamatan berkendara lalu lintas.</p>
+                </div>
+            </div>
+            <div class="row gx-5">
+                <div class="py-5">
+                    <canvas id="myChart" width="500" height="500"></canvas>
+                </div>
+            </div>
+        </div>
+        
+        <!-- Penanganan section-->
+        <section class="py-5 bg-warning bg-opacity-25">
+            <div class="container px-5 my-5">
+                <div class="row gx-5 justify-content-center">
+                    <div class="col-lg-8 col-xl-6">
+                        <div class="text-center">
+                            <h2 class="fw-bolder">Langkah menangani kecelakaan</h2>
+                            <p class="lead fw-normal text-muted mb-5">Lorem ipsum, dolor sit amet consectetur
+                                adipisicing elit. Eaque fugit ratione dicta mollitia. Officiis ad.</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- contoh -->
+                <div>
+                    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                          <div class="carousel-item active mx-auto">
+                            <div class="card border-0 shadow rounded-3 overflow-hidden">
+                                <div class="card-body p-0">
+                                    <div class="row gx-0">
+                                        <div class="col-lg-6 col-xl-5 py-lg-5">
+                                            <div class="p-4 p-md-5">
+                                                <div class="badge bg-primary bg-gradient rounded-pill mb-2">News</div>
+                                                <div class="h2 fw-bolder">Article heading goes here</div>
+                                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique delectus ab doloremque, qui doloribus ea officiis...</p>
+                                                <a class="stretched-link text-decoration-none" href="#!">
+                                                    Read more
+                                                    <i class="bi bi-arrow-right"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6 col-xl-7"><div class="bg-featured-blog" style="background-image: url('https://dummyimage.com/700x350/343a40/6c757d')"></div></div>
+                                    </div>
+                                </div>
+                            </div>
+                          </div>
+                          <div class="carousel-item mx-auto">
+                            <div class="card border-0 shadow rounded-3 overflow-hidden">
+                                <div class="card-body p-0">
+                                    <div class="row gx-0">
+                                        <div class="col-lg-6 col-xl-5 py-lg-5">
+                                            <div class="p-4 p-md-5">
+                                                <div class="badge bg-primary bg-gradient rounded-pill mb-2">News</div>
+                                                <div class="h2 fw-bolder">Article heading goes here</div>
+                                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique delectus ab doloremque, qui doloribus ea officiis...</p>
+                                                <a class="stretched-link text-decoration-none" href="#!">
+                                                    Read more
+                                                    <i class="bi bi-arrow-right"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6 col-xl-7"><div class="bg-featured-blog" style="background-image: url('https://dummyimage.com/700x350/343a40/6c757d')"></div></div>
+                                    </div>
+                                </div>
+                            </div>
+                          </div>
+                          <div class="carousel-item mx-auto">
+                            <div class="card border-0 shadow rounded-3 overflow-hidden">
+                                <div class="card-body p-0">
+                                    <div class="row gx-0">
+                                        <div class="col-lg-6 col-xl-5 py-lg-5">
+                                            <div class="p-4 p-md-5">
+                                                <div class="badge bg-primary bg-gradient rounded-pill mb-2">News</div>
+                                                <div class="h2 fw-bolder">Article heading goes here</div>
+                                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique delectus ab doloremque, qui doloribus ea officiis...</p>
+                                                <a class="stretched-link text-decoration-none" href="#!">
+                                                    Read more
+                                                    <i class="bi bi-arrow-right"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6 col-xl-7"><div class="bg-featured-blog" style="background-image: url('https://dummyimage.com/700x350/343a40/6c757d')"></div></div>
+                                    </div>
+                                </div>
+                            </div>
+                          </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                          <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                          <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <!-- Footer-->
+    <footer class="py-4 mt-auto bg-dark">
+        <div class="container px-5">
+            <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                <div class="col-auto">
+                    <div class="small m-0 text-white"><span class="fw-bold">InfoKecelakaan</span> &copy; 2023</div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Bootstrap core JS-->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4"></script>
+    <script>
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ['Aceh', 'Sumatera Utara', 'Sumatera Barat'],
+                // labels: ['Aceh', 'Sumatera Utara', 'Sumatera Barat', 'Riau', 'Jambi', 'Sumatera Selatan', 'Bengkulu', 'Lampung', 'Kepulauan Bangka Belitung', 'Kepulauan Riau', 'Kalimantan Barat', 'Kalimantan Tengah', 'Kalimantan Selatan', 'Kalimantan Timur', 'Kalimantan Utara', 'Banten', 'DKI Jakarta', 'Jawa Barat', 'Jawa Tengah', 'DI Yogyakarta', 'Jawa Timur', 'Bali', 'Nusa Tenggara Timur', 'Nusa Tenggara Barat', 'Gorontalo', 'Sulawesi Utara', 'Sulawesi Tengah', 'Sulawesi Selatan', 'Sulawesi Tenggara', 'Sulawesi Barat', 'Maluku', 'Maluku Utara', 'Papua Barat', 'Papua'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [
+                        12, 19, 3
+                    ],
+                    // data: [
+                    //     12, 19, 3, 5, 12, 12, 12, 23, 24, 34,
+                    //     34, 23, 12, 13, 14, 14, 14, 14, 14, 14,
+                    //     14, 14, 14, 14, 14, 14, 14, 14, 14, 14,
+                    //     14, 14, 14, 14
+                    // ],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                legend: {
+                    position: 'right',
+                },
+                title: {
+                    display: true,
+                    text: 'Data kecelakaan Tahun 2022'
+                },
+                animation: {
+                    animateScale: true,
+                    animateRotate: true
+                }
+            }
+        });
+    </script>
+
+</body>
+
+</html>
+
+
+
